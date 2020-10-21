d952fdcd555bf82efe4d849b84fcc3802a2f7e25
kip_parcell <kipluis@gmail.com>
2020-10-21 13:08:12 -0700

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,23 @@
-def unsafe?(speed)
 
+# use if/else to determine this method
+
+def unsafe?(speed)
+    if speed > 60
+        return true
+    elsif speed < 40
+        return true
+    else
+        return false
+    end
+# return true if below 40 or over 60
 end
 
-
+# use ternary operator (?:) to determine this method
+# return true if below 40 or over 60
 
 def not_safe?(speed)
-	
+    speed > 60 || speed < 40 ? true : false
+   
 end
 	
 
